e856495d33d23bc3e4fa0b55b3ae35a7d1d248a5
Jordan Lassalle <lassalle.jordan@gmail.com>
2022-12-18 16:24:04 +0100

    Added a filter to show only boat-related closures or maintenance-related closures, and events are shown only if they occur after the fixed date

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,32 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+let toggle= document.getElementById('toggle-listener')
+let maintenanceTableLine=document.getElementsByClassName('maintenanceReason')
+let boatTableLine=document.getElementsByClassName('boatReason')
+console.log('HHHAAAAA');
+toggle.addEventListener("click",function() {
+    for(let i=0; i < maintenanceTableLine.length; i++ ) {
+        console.log(maintenanceTableLine[i])
+        if (maintenanceTableLine[i].classList.contains("hidden")) {
+            maintenanceTableLine[i].classList.remove("hidden")
+            maintenanceTableLine[i].classList.add("shown")
+
+        } else {
+            maintenanceTableLine[i].classList.remove("shown")
+            maintenanceTableLine[i].classList.add("hidden")
+        }
+    }
+    for(let i=0; i < boatTableLine.length; i++ ){
+        if (boatTableLine[i].classList.contains("shown")) {
+            boatTableLine[i].classList.remove("shown")
+            boatTableLine[i].classList.add("hidden")
+
+        } else {
+            boatTableLine[i].classList.remove("hidden")
+            boatTableLine[i].classList.add('shown')
+        }
+    }
+})
+
+
--- src/Service/DataFormatter.php
+++ src/Service/DataFormatter.php
@@ -6,21 +6,33 @@ use DateTime;
 
 class DataFormatter
 {
-    public function formatDataFromApi(array $dataFromApi): array
+    public function formatDataFromApi(array $dataFromApi, Datetime $fixedDate): array
     {
         $arrayOfEvent = [];
         foreach ($dataFromApi as $data) {
+
             $passageDateClosureString = $data['fields']['date_passage'] . ' ' . $data['fields']['fermeture_a_la_circulation'];
             $passageDateOpeningString = $data['fields']['date_passage'] . ' ' . $data['fields']['re_ouverture_a_la_circulation'];
             $reason = $data['fields']['bateau'];
+
             $passageDateClosure = new DateTime($passageDateClosureString);
             $passageDateOpening = new DateTime($passageDateOpeningString);
-            $closureEvent = [
-                'closureHour' => $passageDateClosureString,
-                'reOpenHour' => $passageDateOpeningString,
-                'reason' => $reason,
-            ];
-            $arrayOfEvent[] = $closureEvent;
+            if($passageDateOpening>$fixedDate) {
+                if ($passageDateClosure>$passageDateOpening) {
+                    $passageDateOpening=$passageDateOpening->modify('+1 day');
+                }
+
+                $passageDateClosure = $passageDateClosure->format("d-m-Y à H:i:s");
+                $passageDateOpening = $passageDateOpening->format("d-m-Y à H:i:s");
+                $closureEvent = [
+                    'closureHour' => $passageDateClosure,
+                    'reOpenHour' => $passageDateOpening,
+                    'reason' => $reason,
+                ];
+                $arrayOfEvent[] = $closureEvent;
+            }
+
+
         }
         return $arrayOfEvent;
     }
